d6eb30c73fe4909a70ba86149b58d40932a7345e
Atsushi Yoshinari <7158259+yoshinarl@users.noreply.github.com>
2020-06-07 16:21:45 +0900

    Resolve ITP1_1_C

---
+++ itp1/1_c.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b); 
+    printf("%d %d\n", a * b, 2 * (a + b));
+    return 0;
+}
