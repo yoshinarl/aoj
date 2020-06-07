db8c6dc5d8c5798e18ef51c67ef3b936faad8792
Atsushi Yoshinari <7158259+yoshinarl@users.noreply.github.com>
2020-06-07 16:56:29 +0900

    Resolve ITP1_1_D

---
+++ itp1/1_d.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    int s;
+    scanf("%d", &s); 
+    printf ("%d:%d:%d\n", s / 3600, s % 3600 / 60, s % 60);
+    return 0;
+}
